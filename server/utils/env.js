aafff243582450f9ef675b5df1a20e0e0a1fbd72
kevinpatel-2205 <kevinpatel37592@gmail.com>
2026-03-17 12:39:15 +0530

    chore: integrate Nginx for serving and routing the application

--- server/utils/env.js
+++ server/utils/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 5000;
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+export const CLIENT_URL = process.env.CLIENT_URL;
 
 export const CURRENCY = process.env.CURRENCY;
 
